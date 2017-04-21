5eef1c88f7b028bfc9350b59d1ad304a5ec89cc3
kerri alison <kerrivarner@gmail.com>
2017-04-21 08:13:22 -0400

    Create function.js

---
+++ function.js
@@ -0,0 +1,47 @@
+function getUserChoice() {
+  var userInput = prompt('What is your name?');
+  userInput = userInput.toLowerCase();
+  if(userInput==='rock' || userInput==='paper' || userInput==='scissors') {
+    return userInput;
+}else {
+  console.log('Error!');
+}
+}
+function getComputerChoice() {
+  Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+                      }
+}
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice){
+    return  'Game is a tie!';
+  }
+  if(userChoice === 'rock') {
+    if (computerChoice === 'scissors') {
+    return 'You won!';
+  }else {
+    return 'Computer won!';
+  }
+    
+  }else if (userChoice === 'scissors' && computerChoice === 'rock') {
+    return 'Computer won!';
+  }else if (userChoice === 'rock' && computerChoice === 'paper') {
+    return 'Computer won!';
+  }else if (userChoice === 'paper' && computerChoice === 'rock') {
+    return 'You won!';
+}
+}
+function playGame () {
+  var userChoice = getUserChoice();
+  var computerChoice = getComputerChoice();
+  console.log('You threw: ' + userChoice);
+  console.log('Computer threw: ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+}
+playGame();
